f8499df02e870f3c9cca345b4edc5acf0afe1919
Ezekiel Enns <ezekielenns@pop-os.localdomain>
2023-02-01 16:53:02 -0400

    data structures are defined, storage is defined might need to tweak how transcripts are stored but we will see

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from dataclasses import dataclass
-import json, io, os
+from youtube_transcript_api import YouTubeTranscriptApi
 import googleapiclient.discovery
 import googleapiclient.errors
 from google.oauth2 import service_account
-from googleapiclient.http import MediaIoBaseDownload
-#storeage will be hdF5
-#https://github.com/telegraphic/hickle
-#https://github.com/telegraphic/hickle#dumping-custom-objects
+#storeage will be using cerializing objects via (pickle) and storing the objects in diffrent 
+#hdf5 groups, we can then export it out as a graphql api on flask 
+
+#https://github.com/jdepoix/youtube-transcript-api
+#https://docs.python.org/3/library/pickle.html
+#https://docs.h5py.org/en/stable/quick.html
+#graph ql tut https://www.apollographql.com/blog/graphql/python/complete-api-guide/
+#https://docs.python.org/3/library/csv.html
+
+# ds for this is defined bellow with a special case for video transcripts defined bellow
+# transcripts are stored in a group labed t-{video ID} showing only the text content 
+
 @dataclass
 class Comment:
 #https://developers.google.com/youtube/v3/docs/commentThreads#resource-representation
@@ -57,9 +65,7 @@ class Channel:
     counts:ChannelTotals
     videos:set[Video] #populated
 
-#so this is a unstable way to get them https://github.com/jdepoix/youtube-transcript-api
-#from youtube_transcript_api import YouTubeTranscriptApi
-#test = YouTubeTranscriptApi.get_transcript('T1MWP87Hdk4')
+#so this is a unstable way to get them #test = YouTubeTranscriptApi.get_transcript('T1MWP87Hdk4')
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 SERVICE_ACCOUNT_FILE = './creds.json'
@@ -68,7 +74,7 @@ cred = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FIL
 
 api_service_name = "youtube"
 api_version = "v3"
-#TODO add with for errr halding 
+#TODO add with for error halding 
 yt = googleapiclient.discovery.build(api_service_name,api_version,credentials=cred)
 
 
